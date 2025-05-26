8d87110ec3ae1d96f232b7f0d48273f283830fe2
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-05-26 14:20:24 +1200

    chore: set up tests to run

---
+++ api2/tests/riskRating.test.js
@@ -0,0 +1,11 @@
+//___ RISK RATING TESTS ___
+
+import { keywords } from "../functions/inputData";
+
+test.todo("Claim history includes an empty string and returns a risk rating of 1");
+test.todo("Claim history contains Null and returns risk rating of 1");
+test.todo("Claim history includes 2 keywords and returns a risk rating of 2");
+test.todo("Claim history includes 3 keywords and returns a risk rating of 3");
+test.todo("Claim history includes 4 keywords and returns a risk rating of 4");
+test.todo("Claim history includes 5 keywords and returns a risk rating of 5");
+
